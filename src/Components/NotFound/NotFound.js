77b94c70f9198e0a8525e1f641df1eed89e51c38
faisal-2d <abdullah71faisal@gmail.com>
2022-04-17 00:08:55 +0300

    Image added in Not Found page

--- src/Components/NotFound/NotFound.js
+++ src/Components/NotFound/NotFound.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import './NotFound.css'
 
 const NotFound = () => {
     return (
-        <div>
-            <h3>404 not found</h3>
+        <div className='text-center notfound'>
+            <h1 className='text-danger'>Page Not Found : 404</h1>
+            <img src="https://st.depositphotos.com/1064044/3045/i/600/depositphotos_30459723-stock-photo-broken-cup.jpg" alt="" />
         </div>
     );
 };
