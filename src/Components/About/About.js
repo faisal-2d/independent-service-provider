0ece5a776ede546f8272f41dd0f63672ca52886a
faisal-2d <abdullah71faisal@gmail.com>
2022-04-18 20:55:18 +0300

    about page updated

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -1,11 +1,26 @@
-import React from 'react';
+import React from "react";
+import profilephoto from "../../profilephoto.jpg";
 
 const About = () => {
-    return (
+  return (
+    <div>
+        <h3 className="text center">My goals in 2022</h3>
+      <div className="container d-flex align-items-center">
         <div>
-            <h3>This is about page</h3>
+          <img className="w-25" src={profilephoto} alt="" />
         </div>
-    );
+
+        <div className="w-25">
+          <ul className="list-group list-group-numbered">
+            <li className="list-group-item">Become an web developer</li>
+            <li className="list-group-item">Work as a frrelancer</li>
+            <li className="list-group-item">Start my Own Bussiness</li>
+            
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default About;
